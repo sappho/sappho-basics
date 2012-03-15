5ce2a3f762c7f5d7804a7aef144f7b169f727eee
Andrew Heald <andrew@heald.co.uk>
2012-03-15 11:34:57 +0000

    Bump version.

--- lib/sappho-basics/version.rb
+++ lib/sappho-basics/version.rb
@@ -5,7 +5,7 @@
 
 module Sappho
   NAME = 'sappho-basics'
-  VERSION = '0.0.2'
+  VERSION = '0.0.3'
   AUTHORS = ['Andrew Heald']
   EMAILS = ['andrew@heald.co.uk']
   HOMEPAGE = 'https://github.com/sappho/sappho-basics/wiki'
